648c86e145c1c6e8d27b29c7469d231b792fb94f
beney.kim <bhkim81@gmail.com>
2016-06-12 17:55:33 +0900

    introduce libchannel-biz library.

---
+++ libchannels_biz/src/main/java/com/lge/notyet/channels/ReservationRequestChannel.java
@@ -0,0 +1,40 @@
+package com.lge.notyet.channels;
+
+import com.lge.notyet.lib.comm.*;
+import com.lge.notyet.lib.comm.mqtt.MqttUri;
+import com.sun.javafx.binding.StringFormatter;
+
+public class ReservationRequestChannel extends RequestChannel {
+
+    private final static String mTopic = "/facility/%d/reservation";
+
+    private int mFacilityId = 0;
+
+    private ReservationRequestChannel(INetworkConnection networkConnection) {
+        super(networkConnection);
+    }
+
+    public ReservationRequestChannel setFacilityId(int facilityId) {
+        mFacilityId = facilityId;
+        return this;
+    }
+
+    public static ReservationRequestChannel build(INetworkConnection networkConnection, int facilityId) {
+        return new ReservationRequestChannel(networkConnection).setFacilityId(facilityId);
+    }
+
+    @Override
+    public Uri getChannelDescription() {
+        return new MqttUri(StringFormatter.format(mTopic, mFacilityId).getValue());
+    }
+
+    @Override
+    public void onResponse(NetworkChannel networkChannel, Uri uri, NetworkMessage message) {
+
+    }
+
+    @Override
+    public void onTimeout(NetworkChannel networkChannel, NetworkMessage message) {
+
+    }
+}
