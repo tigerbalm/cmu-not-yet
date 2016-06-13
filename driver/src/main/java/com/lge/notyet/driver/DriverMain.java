825989266f06fc2c0b74e01b863875f01f325d8d
beney.kim <bhkim81@gmail.com>
2016-06-14 07:08:05 +0900

    Under drawing driver UI

--- driver/src/main/java/com/lge/notyet/driver/DriverMain.java
+++ driver/src/main/java/com/lge/notyet/driver/DriverMain.java
@@ -4,15 +4,30 @@ package com.lge.notyet.driver;
  * Created by beney.kim on 2016-06-12.
  */
 
-import com.lge.notyet.driver.manager.NetworkConnectionManager;
-import com.lge.notyet.driver.ui.*;
+import com.lge.notyet.driver.manager.ScreenManager;
+
+import javax.swing.*;
 
 public class DriverMain {
 
-    public static void main(String[] args) {
-        DriverApplicationMainFrame dialog = new DriverApplicationMainFrame();
-        dialog.pack();
-        dialog.setVisible(true);
-        System.exit(0);
+    private final ScreenManager mScreenManager;
+
+    public DriverMain() {
+
+        mScreenManager = ScreenManager.getInstance();
+        JFrame frame = new JFrame("SurePark");
+
+        frame.getContentPane().add(mScreenManager.init());
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
+
+    public static void main(String[] args) {
+        // Start Driver Application
+        new DriverMain();
+    }
+
 }
+
---
+++ driver/src/main/java/com/lge/notyet/driver/manager/ScreenManager.java
@@ -0,0 +1,61 @@
+package com.lge.notyet.driver.manager;
+
+/**
+ * Created by beney.kim on 2016-06-12.
+ */
+
+import com.lge.notyet.driver.ui.*;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ScreenManager {
+
+    private static ScreenManager sScreenManager = null;
+    private CardLayout mMainCardLayout;
+    private JPanel mCards;
+
+    private LoginPanel mLoginPanel;
+    private ReservationPanel mReservationPanel;
+
+    private ScreenManager () {
+        mMainCardLayout = new CardLayout();
+    }
+
+    public static ScreenManager getInstance() {
+        synchronized (ScreenManager.class) {
+            if (sScreenManager == null) {
+                sScreenManager = new ScreenManager();
+            }
+        }
+        return sScreenManager;
+    }
+
+    public JPanel init() {
+
+        mCards = new JPanel(mMainCardLayout);
+
+        mLoginPanel = new LoginPanel();
+        mReservationPanel = new ReservationPanel();
+
+        mCards.add(mLoginPanel.getRootPanel(), mLoginPanel.getName());
+        mCards.add(mReservationPanel.getRootPanel(), mReservationPanel.getName());
+
+        showLoginScreen();
+
+        return mCards;
+    }
+
+    public CardLayout getLayout() {
+        return mMainCardLayout;
+    }
+
+    public void showLoginScreen() {
+        mMainCardLayout.show(mCards, mLoginPanel.getName());
+    }
+
+    public void showReservationScreen() {
+        mMainCardLayout.show(mCards, mReservationPanel.getName());
+    }
+
+}
