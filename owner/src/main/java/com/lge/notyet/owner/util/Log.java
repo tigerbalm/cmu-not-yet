c8a98cd4166523355b33e0eb2d8ab1f4dc4c73cc
beney.kim <bhkim81@gmail.com>
2016-06-16 06:30:37 +0900

    [onwer] Add Login screen and logic

---
+++ owner/src/main/java/com/lge/notyet/owner/util/Log.java
@@ -0,0 +1,19 @@
+package com.lge.notyet.owner.util;
+
+public class Log {
+
+    private static final boolean LOGV = false;
+
+    public static void log(String tag, String log) {
+        logd(tag, log);
+    }
+
+    public static void logd(String tag, String log) {
+        System.out.println(System.currentTimeMillis() + " [" + tag + "] " + log);
+    }
+
+    public static void logv(String tag, String log) {
+        if (!LOGV) return;
+        System.out.println(System.currentTimeMillis() + " [" + tag + "] " + log);
+    }
+}
