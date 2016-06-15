acb36aa839c13e54a58dd9c507ffb8871b700c70
beney.kim <bhkim81@gmail.com>
2016-06-16 07:16:58 +0900

    [attendant] add facility update logic

---
+++ attendant/src/main/java/com/lge/notyet/attendant/ui/FacilityMonitorPanel.java
@@ -0,0 +1,22 @@
+package com.lge.notyet.attendant.ui;
+
+import javax.swing.*;
+
+/**
+ * Created by beney.kim on 2016-06-16.
+ */
+public class FacilityMonitorPanel {
+    private JPanel mForm;
+
+
+    public void init() {
+    }
+
+    public JPanel getRootPanel() {
+        return mForm;
+    }
+
+    public String getName() {
+        return "FacilityMonitorPanel";
+    }
+}
