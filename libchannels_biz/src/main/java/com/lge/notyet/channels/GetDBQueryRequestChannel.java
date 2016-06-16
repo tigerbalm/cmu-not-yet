d0c66eee37ed199a2c20e62ae4d1da806767332d
gladvindurai <gladvin.durai@gmail.com>
2016-06-16 13:18:19 -0400

    Adding Server communication via MQTT

---
+++ libchannels_biz/src/main/java/com/lge/notyet/channels/GetDBQueryRequestChannel.java
@@ -0,0 +1,40 @@
+package com.lge.notyet.channels;
+
+import com.eclipsesource.json.JsonObject;
+import com.lge.notyet.lib.comm.ClientChannelRegistry;
+import com.lge.notyet.lib.comm.INetworkConnection;
+import com.lge.notyet.lib.comm.NetworkMessage;
+import com.lge.notyet.lib.comm.Uri;
+import com.lge.notyet.lib.comm.mqtt.MqttNetworkMessage;
+import com.lge.notyet.lib.comm.mqtt.MqttUri;
+import com.sun.javafx.binding.StringFormatter;
+
+public class GetDBQueryRequestChannel extends ClientChannelRegistry {
+    private final static String TOPIC = "/facility/dbquery/get";
+    private final static String KEY_SESSION_KEY = "session_key";
+    private final static String KEY_DBQUERY_KEY = "dbquery_key";
+
+    public GetDBQueryRequestChannel(INetworkConnection networkConnection) {
+        super(networkConnection);
+    }
+
+    @Override
+    public Uri getChannelDescription() {
+        return new MqttUri(StringFormatter.format(TOPIC).getValue());
+    }
+
+    public static String getSessionKey(NetworkMessage networkMessage) {
+        return ((JsonObject) networkMessage.getMessage()).get(KEY_SESSION_KEY).asString();
+    }
+
+    public static String getKeyDbqueryKey(NetworkMessage networkMessage) {
+        return ((JsonObject) networkMessage.getMessage()).get(KEY_DBQUERY_KEY).asString();
+    }
+
+    public static MqttNetworkMessage createRequestMessage(String sessionKey, String dbQueryKey) {
+        JsonObject requestObject = new JsonObject();
+        requestObject.add(KEY_SESSION_KEY, sessionKey);
+        requestObject.add(KEY_DBQUERY_KEY, dbQueryKey);
+        return new MqttNetworkMessage(requestObject);
+    }
+}
---
+++ owner/src/main/java/com/lge/notyet/owner/business/dbQueryTask.java
@@ -0,0 +1,68 @@
+package com.lge.notyet.owner.business;
+
+import com.lge.notyet.channels.LoginRequestChannel;
+import com.lge.notyet.lib.comm.*;
+import com.lge.notyet.lib.comm.mqtt.MqttNetworkMessage;
+import com.lge.notyet.owner.manager.NetworkConnectionManager;
+import com.lge.notyet.owner.ui.ITaskDoneCallback;
+import com.lge.notyet.owner.util.Log;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+
+
+public class dbQueryTask implements Callable<Void> {
+
+    private static final String LOG_TAG = "dbQueryTask";
+
+    private String mUserEmailAddress;
+    private String mPassWord;
+    private ITaskDoneCallback mTaskDoneCallback;
+
+    public dbQueryTask(String userEmailAddress, String passWord, ITaskDoneCallback taskDoneCallback) {
+        mUserEmailAddress = userEmailAddress;
+        mPassWord = passWord;
+        mTaskDoneCallback = taskDoneCallback;
+    }
+
+    @Override
+    public Void call() throws Exception {
+
+        NetworkConnectionManager ncm = NetworkConnectionManager.getInstance();
+        ncm.open();
+        LoginRequestChannel lc = ncm.createLoginChannel();
+        lc.addObserver(mLoginResult);
+        lc.addTimeoutObserver(mLoginTimeout);
+
+        MqttNetworkMessage requestMsg = lc.createRequestMessage(mUserEmailAddress, mPassWord);
+        Log.log(LOG_TAG, requestMsg.toString());
+        lc.request(requestMsg);
+        return null;
+    }
+
+    // Business Logic here, we have no time :(
+    private IOnResponse mLoginResult = new IOnResponse() {
+
+        @Override
+        public void onResponse(NetworkChannel networkChannel, Uri uri, NetworkMessage message) {
+
+            // Need to parse
+            // ReservationResponseMessage result = (ReservationResponseMessage) message;
+            System.out.println("mLoginResult Result=" + message.getMessage());
+            mTaskDoneCallback.onDone(ITaskDoneCallback.SUCCESS, message);
+        }
+    };
+
+    private IOnTimeout mLoginTimeout = new IOnTimeout() {
+
+        @Override
+        public void onTimeout(NetworkChannel networkChannel, NetworkMessage message) {
+            System.out.println("Failed to send Message=" + message);
+            mTaskDoneCallback.onDone(ITaskDoneCallback.FAIL, null);
+        }
+    };
+
+    public static FutureTask<Void> getTask(String userEmailAddress, String passWord, ITaskDoneCallback taskDoneCallback) {
+        return new FutureTask<>(new dbQueryTask(userEmailAddress, passWord, taskDoneCallback));
+    }
+}
