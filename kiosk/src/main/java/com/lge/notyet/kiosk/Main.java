d22498bed5feb9986e2539bc5b7468400f4fb316
sjun.lee <sjun.lee@lge.com>
2016-06-16 02:19:05 -0400

    [Kiosk] initial version

---
+++ kiosk/src/main/java/com/lge/notyet/kiosk/Main.java
@@ -0,0 +1,13 @@
+package com.lge.notyet.kiosk;
+
+import com.fazecast.jSerialComm.SerialPort;
+import com.lge.notyet.kiosk.ui.KioskGUI;
+
+/**
+ * Created by sjun.lee on 2016-06-09.
+ */
+public class Main {
+    public static void main(String[] args) {
+        KioskGUI.showGui();
+    }
+}
