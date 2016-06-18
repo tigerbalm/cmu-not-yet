0721ee3665606f5b371b50bd115f0ad46fdf0eba
reshout <reshout@gmail.com>
2016-06-17 21:56:21 -0400

    [server] refactoring is being performed, partially done for AuthenticationManager

--- server/src/main/java/com/lge/notyet/server/verticle/MainVerticle.java
+++ server/src/main/java/com/lge/notyet/server/verticle/MainVerticle.java
@@ -1,9 +1,17 @@
 package com.lge.notyet.server.verticle;
 
+import com.eclipsesource.json.JsonObject;
+import com.lge.notyet.channels.LoginRequestChannel;
+import com.lge.notyet.channels.LoginResponseChannel;
+import com.lge.notyet.channels.SignUpRequestChannel;
+import com.lge.notyet.channels.SignUpResponseChannel;
+import com.lge.notyet.lib.comm.INetworkConnection;
+import com.lge.notyet.lib.comm.NetworkMessage;
 import com.lge.notyet.server.manager.AuthenticationManager;
 import com.lge.notyet.server.manager.ReservationManager;
 import com.lge.notyet.server.manager.FacilityManager;
 import com.lge.notyet.server.manager.StatisticsManager;
+import com.lge.notyet.server.model.User;
 import com.lge.notyet.server.proxy.CommunicationProxy;
 import com.lge.notyet.server.proxy.DatabaseProxy;
 import io.vertx.core.*;
@@ -13,6 +21,11 @@ public class MainVerticle extends AbstractVerticle {
     private static final String DB_HOST = "localhost";
     private static final String DB_USERNAME = "dba";
     private static final String DB_PASSWORD = "dba";
+
+    private AuthenticationManager authenticationManager;
+    private ReservationManager reservationManager;
+    private FacilityManager facilityManager;
+    private StatisticsManager statisticsManager;
 
     private DatabaseProxy databaseProxy;
     private CommunicationProxy communicationProxy;
@@ -28,6 +41,7 @@ public class MainVerticle extends AbstractVerticle {
             if (ar.succeeded()) {
                 startFuture.complete();
                 startManagers();
+                listenChannels();
             } else {
                 startFuture.fail(ar.cause());
             }
@@ -35,10 +49,46 @@ public class MainVerticle extends AbstractVerticle {
     }
 
     private void startManagers() {
-        AuthenticationManager.getInstance();
-        ReservationManager.getInstance();
-        FacilityManager.getInstance();
-        StatisticsManager.getInstance();
+        authenticationManager = AuthenticationManager.getInstance();
+        reservationManager = ReservationManager.getInstance();
+        facilityManager = FacilityManager.getInstance();
+        statisticsManager = StatisticsManager.getInstance();
+    }
+
+    private void listenChannels() {
+        final INetworkConnection networkConnection = communicationProxy.getNetworkConnection();
+
+        new SignUpResponseChannel(networkConnection).addObserver((networkChannel, uri, message) -> signUp(message)).listen();
+        new LoginResponseChannel(networkConnection).addObserver((networkChannel, uri, message) -> login(message)).listen();
+    }
+
+    private void login(NetworkMessage message) {
+        final String email = LoginRequestChannel.getEmail(message);
+        final String password = LoginRequestChannel.getPassword(message);
+
+        authenticationManager.getEmailPasswordUser(email, password, ar -> {
+            if (ar.failed()) {
+                communicationProxy.responseFail(message, ar.cause());
+            } else {
+                JsonObject userObject = ar.result();
+                communicationProxy.responseSuccess(message, userObject);
+            }
+        });
+    }
+
+    private void signUp(NetworkMessage message) {
+        final String email = SignUpRequestChannel.getEmail(message);
+        final String password = SignUpRequestChannel.getPassword(message);
+        final String cardNumber = SignUpRequestChannel.getCardNumber(message);
+        final String cardExpiration = SignUpRequestChannel.getCardExpiration(message);
+
+        authenticationManager.signUp(email, password, cardNumber, cardExpiration, User.USER_TYPE_DRIVER, ar1 -> {
+            if (ar1.failed()) {
+                communicationProxy.responseFail(message, ar1.cause());
+            } else {
+                communicationProxy.responseSuccess(message);
+            }
+        });
     }
 
     @Override
