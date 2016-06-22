69519b145098bcfcbdedaf7d62ee1f271e2eda9a
reshout <reshout@gmail.com>
2016-06-22 14:48:13 -0400

    [libcrypto] SureParkCrypto bug fix

--- libcrypto/src/main/java/com/lge/notyet/lib/crypto/SureParkCrypto.java
+++ libcrypto/src/main/java/com/lge/notyet/lib/crypto/SureParkCrypto.java
@@ -3,11 +3,19 @@ package com.lge.notyet.lib.crypto;
 public class SureParkCrypto {
     private static final String SEED = "*2470C0C06DEE42FD1618BB99005ADCA2EC9D1E19";
 
-    public static String encrypt(String seed, String cleartext) throws Exception {
-        return Crypto.encrypt(seed, cleartext);
+    public static String encrypt(String cleartext) {
+        try {
+            return Crypto.encrypt(SEED, cleartext);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public static String decrypt(String seed, String encrypted) throws Exception {
-        return Crypto.decrypt(seed, encrypted);
+    public static String decrypt(String encrypted) {
+        try {
+            return Crypto.decrypt(SEED, encrypted);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
